8786ae84303b821e72575dd06d0cd63aaab3fe87
William Dye <will2dye4@mac.com>
2019-03-16 12:44:03 -0400

    Switched to grid layout, updates to UI

--- jeopardy_ui.py
+++ jeopardy_ui.py
@@ -8,6 +8,7 @@ from collections import namedtuple
 from tkinter import font
 from tkinter import ttk
 from multiprocessing import Process, Queue
+from threading import RLock
 
 from jeopardy_cli import ClientApp
 from jeopardy_client import JeopardyClient
@@ -23,6 +24,7 @@ TaggedText = namedtuple('TaggedText', ['text', 'tags'])
 class JeopardyApp(ttk.Frame):
 
     DEFAULT_TICK_DELAY_MILLIS = 100
+    FONT_FAMILY = 'Consolas'  # was Courier
     HOST = 'Host'
 
     DARK_GRAY = '#333333'
@@ -34,6 +36,7 @@ class JeopardyApp(ttk.Frame):
     def __init__(self, master=None, server_address=None, nick=None, dark_mode=False):
         if master is None:
             master = tk.Tk()
+            master.minsize(width=400, height=300)
             master.title('Jeopardy!')
         super().__init__(master)
         master.protocol('WM_DELETE_WINDOW', self.close)
@@ -45,18 +48,26 @@ class JeopardyApp(ttk.Frame):
         self.players = {}
         self.client = JeopardyClient(server_address, self.player_id)
         self.current_question_id = None
+        self.lock = RLock()
         self.port = random.randrange(65000, 65536)
         self.app_process = None
         self.event_queue = Queue(maxsize=100)
         self.stats_queue = Queue(maxsize=100)
 
+        # enable resizing
+        top = self.winfo_toplevel()
+        top.rowconfigure(0, weight=1)
+        top.columnconfigure(0, weight=1)
+        self.rowconfigure(0, weight=1)
+        self.columnconfigure(0, weight=1)
+
         self.stats_pane = None
         self.event_pane = None
-        self.pack(fill='both', expand=True)
+        self.grid(sticky=tk.N + tk.S + tk.E + tk.W)
         self.dark_mode = dark_mode
         self.configure_style()
-        self.default_font = font.Font(self, 'Courier')
-        self.bold_font = font.Font(self, 'Courier')
+        self.default_font = font.Font(self, self.FONT_FAMILY)
+        self.bold_font = font.Font(self, self.FONT_FAMILY)
         self.bold_font.configure(weight='bold')
         self.main_pane = self.create_main_pane()
         self.input_text = tk.StringVar(value='')
@@ -64,25 +75,32 @@ class JeopardyApp(ttk.Frame):
 
     def configure_style(self):
         style = ttk.Style()
+        style.configure('TEntry', borderwidth=0, highlightthickness=0)
+        style.configure('TScrollbar', borderwidth=0)
+        style.configure('Line.TFrame', background='black', foreground='black')
         if self.dark_mode:
-            style.configure('Main.TFrame', background=self.DARK_GRAY)
+            style.configure('Main.TFrame', background=self.DARK_GRAY, borderwidth=0, highlightthickness=0)
         else:
-            style.configure('Main.TFrame')
+            style.configure('Main.TFrame', borderwidth=0, highlightthickness=0)
 
     def create_main_pane(self):
-        main_pane = ttk.Frame(self, height=50, width=120, style='Main.TFrame')
-        main_pane.pack(side='top', fill='both', expand=True)
+        main_pane = ttk.Frame(self, height=600, width=600, style='Main.TFrame')
+        main_pane.grid(row=0, column=0, sticky=tk.N + tk.S + tk.E + tk.W)
         self.stats_pane = self.create_stats_pane(main_pane)
         self.update_stats()
+        vertical_line = ttk.Frame(main_pane, height=600, width=5, style='Line.TFrame')
+        vertical_line.grid(row=0, column=1)
         self.event_pane = self.create_event_pane(main_pane)
+        main_pane.rowconfigure(0, weight=1)
+        main_pane.columnconfigure(2, weight=1)
         return main_pane
 
     def create_stats_pane(self, parent):
-        pane = tk.Text(parent, height=50, width=20, font=self.default_font, background=self.LIGHT_GRAY, state=tk.DISABLED, takefocus=0, undo=False)
+        pane = tk.Text(parent, height=50, width=20, font=self.default_font, background=self.LIGHT_GRAY, borderwidth=0, highlightthickness=0, state=tk.DISABLED, takefocus=0, undo=False)
         pane.tag_configure('heading', font=self.bold_font, background=self.JEOPARDY_BLUE, foreground='white', justify=tk.CENTER, spacing1=6, spacing3=6)
         pane.tag_configure('bold', font=self.bold_font, justify=tk.CENTER)
         pane.tag_configure('centered', justify=tk.CENTER)
-        pane.pack(side='left', fill='both')
+        pane.grid(row=0, column=0, sticky=tk.N + tk.S)
         return pane
 
     def create_event_pane(self, parent):
@@ -92,28 +110,41 @@ class JeopardyApp(ttk.Frame):
         else:
             background = 'white'
             foreground = 'black'
-        pane = tk.Text(parent, height=50, width=80, font=self.default_font, background=background, foreground=foreground, wrap=tk.WORD, state=tk.DISABLED, takefocus=0, undo=False)
+        pane = tk.Text(parent, height=50, width=80, font=self.default_font, background=background, foreground=foreground, borderwidth=0, highlightthickness=0, wrap=tk.WORD, state=tk.DISABLED, takefocus=0, undo=False)
         pane.tag_configure('bold', font=self.bold_font)
-        small_font = font.Font(pane, 'Courier')
+        small_font = font.Font(pane, self.FONT_FAMILY)
         small_font.configure(size=4)
         pane.tag_configure('small', font=small_font)
         pane.tag_configure('question_category', background=self.JEOPARDY_BLUE, font=self.bold_font, foreground='white', justify=tk.CENTER, spacing1=3, spacing3=3)
         pane.tag_configure('question_value', background=self.JEOPARDY_LIGHT_BLUE, foreground='white', justify=tk.CENTER, spacing1=3, spacing3=3)
         pane.tag_configure('question_text', background='white', foreground='black', justify=tk.CENTER, spacing1=3, spacing3=3)
-        tiny_font = font.Font(pane, 'Courier')
+        tiny_font = font.Font(pane, self.FONT_FAMILY)
         tiny_font.configure(size=2)
         pane.tag_configure('line', background='black', font=tiny_font)
-        scrollbar = ttk.Scrollbar(parent, orient=tk.VERTICAL, borderwidth=1, command=pane.yview)
-        scrollbar.pack(side='right', fill='y', expand=False)
+        scrollbar = ttk.Scrollbar(parent, orient=tk.VERTICAL, command=pane.yview)
         pane.configure(yscrollcommand=scrollbar.set)
-        pane.pack(side='right', fill='both', expand=True)
+        pane.grid(row=0, column=2, sticky=tk.N + tk.S + tk.E + tk.W)
+        scrollbar.grid(row=0, column=3, sticky=tk.N + tk.S)
         return pane
 
     def create_input_pane(self):
-        pane = ttk.Entry(self, width=80, textvariable=self.input_text)
+        pane = ttk.Entry(self, width=80, style='TEntry', textvariable=self.input_text)
         pane.bind('<KeyPress-Return>', self.handle_user_input)
-        pane.pack(side='bottom')
+        pane.grid(row=1, column=0, sticky=tk.E + tk.W)
         return pane
+
+    def is_current_question(self, question_id):
+        return self.current_question_id is not None and self.current_question_id == question_id
+
+    def update_current_question(self, question_id):
+        with self.lock:
+            self.current_question_id = question_id
+
+    def maybe_update_and_show_question(self, question):
+        with self.lock:
+            if not self.is_current_question(question.question_id):
+                self.update_current_question(question.question_id)
+                self.show_question(question)
 
     @property
     def longest_player_nick(self):
@@ -211,25 +242,26 @@ class JeopardyApp(ttk.Frame):
             self.append_to_event_pane('Type "/q" to get a new question. Enter your answer to check if it is correct.')
         elif user_input == '/q':
             question = self.client.get_question()
-            if question.question_id != self.current_question_id:
-                self.current_question_id = question.question_id
-                self.show_question(question)
+            self.maybe_update_and_show_question(question)
         elif user_input.startswith('/c '):
             message = user_input[3:]
             self.client.chat(message)
             self.player_says(self.nick, message)
         else:
-            player = self.players[self.player_id]
-            player.total_answers += 1
-            self.player_says(self.nick, f'What is {user_input}?')
-            resp = self.client.answer(user_input)
-            if resp is not None and resp.is_correct:
-                host_response = f'{self.nick}, that is correct.'
-                player.correct_answers += 1
-                player.score += resp.value
+            if self.current_question_id is None:
+                self.host_says(f'{self.nick}, there is currently no active question.')
             else:
-                host_response = f'No, sorry, {self.nick}.'
-            self.host_says(host_response)
+                player = self.players[self.player_id]
+                player.total_answers += 1
+                self.player_says(self.nick, f'What is {user_input}?')
+                resp = self.client.answer(user_input)
+                if resp is not None and resp.is_correct:
+                    host_response = f'{self.nick}, that is correct.'
+                    player.correct_answers += 1
+                    player.score += resp.value
+                else:
+                    host_response = f'No, sorry, {self.nick}.'
+                self.host_says(host_response)
         self.input_text.set('')
 
     def handle(self, event):
@@ -239,15 +271,16 @@ class JeopardyApp(ttk.Frame):
             self.host_says('A new game is starting!')
         elif event.event_type == 'NEW_QUESTION':
             question = Question.from_json(event.payload)
-            if question.question_id != self.current_question_id:
-                self.current_question_id = question.question_id
-                self.show_question(question)
+            self.maybe_update_and_show_question(question)
         elif event.event_type == 'NEW_ANSWER':
             nick = event.player.nick
             answer = event.payload['answer']
-            correct = event.payload['is_correct']
+            if event.payload['is_correct']:
+                host_response = f'{nick}, that is correct.'
+                self.update_current_question(None)
+            else:
+                host_response = f'No, sorry, {nick}.'
             self.player_says(nick, f'What is {answer}?')
-            host_response = f'{nick}, that is correct.' if correct else f'No, sorry, {nick}.'
             self.host_says(host_response)
             self.show_stats_update(event)
         elif event.event_type in {'NEW_PLAYER', 'PLAYER_LEFT'}:
@@ -256,6 +289,7 @@ class JeopardyApp(ttk.Frame):
             self.host_says(f'{nick} has {verb} the game.')
             self.show_stats_update(event)
         elif event.event_type == 'QUESTION_TIMEOUT':
+            self.update_current_question(None)  # TODO - this isn't actually working
             self.host_says(f'The correct answer is: {event.payload["answer"]}')
         elif event.event_type == 'CHAT_MESSAGE':
             nick = event.player.nick
@@ -286,6 +320,12 @@ class JeopardyApp(ttk.Frame):
         self.fetch_stats()
         self.tick()
         self.input_pane.focus_set()
+
+        with self.lock:
+            if self.current_question_id is None:
+                question = self.client.get_question()
+                self.maybe_update_and_show_question(question)
+
         super().mainloop()
 
     def start_app_process(self):
